918b728b588a3e12de33353761b115ba8727cbb3
AlexSmith716 <1691290+alexsmith716@users.noreply.github.com>
2021-06-11 15:10:53 -0400

    feat: Added promise option for loadAboutCSV dispatch.

--- src/containers/AboutCSV/AboutCSV.js
+++ src/containers/AboutCSV/AboutCSV.js
@@ -18,22 +18,22 @@ const AboutCSV = () => {
 	const data = useSelector((state) => state.aboutCSV.data);
 	const [dispatchError, setDispatchError] = useState(null);
 
-  //  const doLoadAboutCsv = async () => {
-  //    try {
-  //      await dispatch(loadAboutCSV());
-  //    } catch (err) {
-  //      setDispatchError(err);
-  //      throw new Error("Error fetching data");
-  //    }
-  //  };
+	//	const doLoadAboutCSV = async () => {
+	//		try {
+	//			await dispatch(loadAboutCSV());
+	//		} catch (err) {
+	//			setDispatchError(err);
+	//			throw new Error("Error fetching data");
+	//		}
+	//	};
 
-  const doLoadAboutCsv = () => {
-    dispatch(loadAboutCSV())
-    .catch(error => {
-      setDispatchError(err);
-      throw new Error("Error fetching data");
-    })
-  };
+	const doLoadAboutCSV = () => {
+		dispatch(loadAboutCSV())
+			.catch(error => {
+				setDispatchError(err);
+				throw new Error("Error fetching data");
+			})
+	};
 
 	useEffect(() => {
 			if (!data) {
